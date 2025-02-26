6d247481cda1a4e37ee3276609d5d627bd484268
Javier Harford <javierbsg@Javiers-MacBook-Pro.local>
2025-02-26 19:56:54 +0000

    finalise the project after reading all of the chapter

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ fn main() {
     let text = matches.values_of_lossy("text").unwrap();
     let omit_newline = matches.is_present("omit_newline");
 
-    println!(
+    print!(
         "{}{}",
         text.join(" "),
         if omit_newline { "" } else { "\n" },
--- tests/cli.rs
+++ tests/cli.rs
@@ -1,4 +1,7 @@
 use assert_cmd::Command;
+use std::fs;
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
 
 #[test]
 fn dies_no_args() {
@@ -8,8 +11,32 @@ fn dies_no_args() {
         .stderr(predicates::str::contains("USAGE"));
 }
 
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echor")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected); 
+    Ok(())
+}
+
 #[test]
-fn runs() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("hello").assert().success();
+fn hello1() -> TestResult {
+    run(&["Hello there"], "tests/expected/hello1.txt")
+}
+
+#[test]
+fn hello2() -> TestResult {
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> TestResult {
+    run(&["Hello  there", "-n"], "tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello2_no_newline() -> TestResult {
+    run(&["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
 }
